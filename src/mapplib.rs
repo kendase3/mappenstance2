192e1b6b8db2a3a7f5b4774fe967fbdc40a9cb27
Steven 'Steve' Kendall <skendall@neverware.com>
2020-11-28 19:56:17 -0500

    added new mapp-level goodies

--- src/mapplib.rs
+++ src/mapplib.rs
@@ -11,3 +11,28 @@ impl fmt::Display for Cell {
         write!(f, "{}", self.c)
     }
 }
+
+// ideally of arbitrary width and height
+// should have a default width of 80 and default height of 20
+pub struct Mapp {
+    width: u32,
+    height: u32,
+}
+
+impl Mapp {
+    fn new(width: u32, height: u32) -> Mapp {
+        Mapp { width, height }
+    }
+    fn default() -> Mapp {
+        Mapp {
+            width: Mapp::get_default_width(),
+            height: Mapp::get_default_height(),
+        }
+    }
+    fn get_default_height() -> u32 {
+        20
+    }
+    fn get_default_width() -> u32 {
+        80
+    }
+}
